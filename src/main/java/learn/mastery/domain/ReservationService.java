609bf1ae29768e50963bd6cbeee721558623dc23
matthew <matthew.almeida@sbcglobal.net>
2023-11-10 14:33:31 -0500

    Reservation service setup

--- src/main/java/learn/mastery/domain/ReservationService.java
+++ src/main/java/learn/mastery/domain/ReservationService.java
@@ -1,4 +1,31 @@
 package learn.mastery.domain;
 
+import learn.mastery.data.ReservationRepository;
+import learn.mastery.models.Host;
+import learn.mastery.models.Reservation;
+
+import java.util.List;
+
 public class ReservationService {
+    private ReservationRepository repository;
+
+    public ReservationService(ReservationRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Reservation> findByHost(Host host) {
+        return null;
+    }
+    public Result add(Reservation reservation) {
+        return null;
+    }
+    public Result update(Reservation reservation) {
+        return null;
+    }
+    public Result delete(Reservation reservation) {
+        return null;
+    }
+    private Result validate(Reservation reservation) {
+        return null;
+    }
 }
